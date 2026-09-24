a388b3927c4b8a6c7afc82cdd01b6d1c93a82a3d
Afshin Ezati <afshinezati9@gmail.com>
2026-09-24 11:56:33 +0330

    chore: remove unused Room migration test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,4 @@ dependencies {
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.room:room-testing:2.7.0")
 }
