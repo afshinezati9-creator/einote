01d97e63751f588812dca384f7e9aa03023fbab8
Afshin Ezati <afshinezati9@gmail.com>
2026-09-24 14:47:24 +0330

    release: bump eiNote to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         minSdk=26
         targetSdk=35
         testInstrumentationRunner="androidx.test.runner.AndroidJUnitRunner"
-        versionCode=8
-        versionName="0.8.0"
+        versionCode=9
+        versionName="1.0.0"
     }
 }
 dependencies {
